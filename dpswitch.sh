144438a678e4954fa99e116d6b6c4cbecc73b8af
Sunny Kahlon <sunny17@gmail.com>
2020-07-02 01:13:00 -0700

    removed extra lines/spaces

--- dpswitch.sh
+++ dpswitch.sh
@@ -1,7 +1,5 @@
 #!bin/bash
-
 # script to switch between X11 and Wayland display server
-
 case $XDG_SESSION_TYPE in
     x11)
     sudo sed -i 's/^WaylandEnable.*/#WaylandEnable=false/' /etc/gdm/custom.conf
