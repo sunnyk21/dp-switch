a21449209965e934b12726ffdd7d3f8c43300be3
Sunny Kahlon <sunny17@gmail.com>
2020-08-19 23:54:33 -0700

    added echo statement and message for no sudo required

--- dpswitch.sh
+++ dpswitch.sh
@@ -1,5 +1,8 @@
-#!bin/bash
+#! /bin/bash
 # script to switch between X11 and Wayland display server
+# execute this without "sudo". it will automatically prompt for sudo password.
+
+echo "current display server: $XDG_SESSION_TYPE"
 case $XDG_SESSION_TYPE in
     x11)
     sudo sed -i 's/^WaylandEnable.*/#WaylandEnable=false/' /etc/gdm/custom.conf
